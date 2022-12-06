fb69bef84a970d86d2b95a06fa7ea9d50d83b1ef
samer-niy <107676762+samer-niy@users.noreply.github.com>
2022-12-06 11:57:31 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,42 +1,47 @@
 from sbot import *
 from time import sleep
 from sbot import BRAKE, COAST
-import Rpi.GPIO as GPIO
-r = Robot(wait_start=False)
+from sbot import GPIOPinMode
 
-while True:
-    r.motor_board.motors[0].power = 0.1
-    r.motor_board.motors[1].power = 0.1
+r = Robot()
+my_arduino = r.arduino
+coordinates = [(675,0),(1350,0),(2025,0),(2700,0),(3375,0),(4050,0),(4725,0),
+               (5400,675),(5400,1350),(5400,2025),(5400,2700),(5400,3375),(5400,4050),(5400,4725),
+               (4725,5400),(4050,5400),(3375,5400),(2700,5400),(2025,5400),(1350,5400),(675,5400),
+               (0,4725),(0,4050),(0,3375),(0,2700),(0,2025),(0,1350),(0,675)]
 
-    sleep(3)
-    r.motor_board.motors[0].power = 0
-    r.motor_board.motors[1].power = 0 # both work??
-    r.motor_board.motors[0].power = BRAKE  # makes power 0 basically
-    r.motor_board.motors[1].power = BRAKE
-    # braking this motor makes the direction change etc right wheel turns off it goes right
 
-    r.motor_board.motors[1].power = COAST # removes power (can still move with momentum)
-    sleep(3)
+found = False
+base_tag = 28
+my_corner_tag = base_tag + r.zone
+territory_tag = []
+dist = 0
+angle = 0
+identity = 0
 
-def turn_right():
-    r.motor_board.motors[0].power = 0.1
-    r.motor_board.motors[1].power = 0
+
+##if my_corner_tag 
+
 
 def turn_left():
-    r.motor_board.motors[0].power = 0
-    r.motor_board.motors[1].power = 0.1
+    r.motor_board.motors[0].power = -0.2
+    r.motor_board.motors[1].power = -0
+
+def turn_right():
+    r.motor_board.motors[0].power = -0
+    r.motor_board.motors[1].power = -0.2
 
 def forward():
-    r.motor_board.motors[0].power = 0.1
-    r.motor_board.motors[1].power = 0.1
+    r.motor_board.motors[0].power = -0.2
+    r.motor_board.motors[1].power = -0.2
 
 def forward_reverse():
-    r.motor_board.motors[0].power = 0.1
-    r.motor_board.motors[1].power = -0.1
+    r.motor_board.motors[0].power = -0.1
+    r.motor_board.motors[1].power = 0.17
 
 def reverse():
-    r.motor_board.motors[0].power = -0.1
-    r.motor_board.motors[1].power = -0.1forward()
+    r.motor_board.motors[0].power = 0.2
+    r.motor_board.motors[1].power = 0.2
 
 def constant_speed():
     r.motor_board.motors[0].power = COAST
@@ -46,64 +51,65 @@ def stop():
     r.motor_board.motors[0].power = BRAKE
     r.motor_board.motors[1].power = BRAKE
 
-try:
-    GPIO.setmode(GPIO.BOARD)
 
-    PIN_TRIGGER = 7
-    PIN_ECHO = 11
 
-    GPIO.setup(PIN_TRIGGER,GPIO.OUT)
-    GPIO.setup(PIN_ECHO,GPIO.IN)
+#sensor = r.arduino.ultrasound_sensors[4, 5]
 
-    GPIO.output(PIN_TRIGGER,GPIO.LOW)
-    print('Waits for sensor to settle')
-    time.sleep(2)
+#pulse_time = sensor.pulse()
 
-    print("Calculating distance")
+#distance = sensor.distance()
+    
+#print(f"time taken for pulse to reflect: {pulse_time}")
+#print(f"distance to object: {distance}")
 
-    GPIO.output(PIN_TRIGGER, GPIO.HIGH)
 
-    time.sleep(0.00001)
 
-    GPIO.output(PIN_TRIGGER, GPIO.LOW)
 
-    while GPIO.input(PIN_ECHO)==0:
-            pulse_start_time = time.time()
-    while GPIO.input(PIN_ECHO)==1:
-            pulse_end_time = time.time()
-
-    pulse_duration = pulse_end_time - pulse_start_time
-
-    distance = round(pulse_duration * 17150, 2)
-
-    print ("Distance:",distance,"cm")
-
-finally:
-    GPIO.cleanup()
-
-while True:
-    centre = getObstacle(0)
-    left = getObstacle(1)
-    right = getObstacle(2)
-    time.sleep(1)
-    forward()
-    constant_speed()
-    if distance < 20:
-        stop()
+#markers = r.camera.see()
+#r.power_board.piezo.buzz(0.5, Note.C6)
+#r.camera.save("snapshot.jpg")
+#
+#for m in markers:
+ #   print("distance", float(m.distance / 10))
+  #  print("rotation", float((m.spherical.rot_y * 180) / 3.14))
+#good = True
+#while good == True:
+   # forward()
+    #sleep(5)
+    #good = False
 
 
 forward()
-constant_speed()
-time.sleep(3)
-turn_right()
-time.sleep(1)
+sleep(4)
 turn_left()
-time.sleep(1)
+sleep(1.5)
 forward()
-constant_speed()
-time.sleep(4)
-stop()
+sleep(2)
+reverse()
+sleep(2)
+turn_left()
+sleep(1.5)
+forward()
+sleep(4.1)
+turn_right()
+sleep(1.7)
+forward()
+sleep(4)
+turn_right()
+sleep(1.5)
+forward()
+sleep(3)
 
+#reverse()
+##sleep(2)
+#turn_left
+#sleep(1)
+#forward()
+#sleep(3)
+#turn_right()
+#sleep(1)
+#forward()
+#sleep(3)
+#forward_reverse()
+#sleep(10)
 
-
-r.waitstart()
